bfb41e101dea16caa3ebd2a4f74e9959eb1ed962
Seif Eldin Mohamed <s.seif26@yahoo.com>
2021-05-24 22:58:36 +0200

    initial backend

---
+++ Backend/freeCodeGram-master/app/ItemDonation.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class ItemDonation extends Model
+{
+    protected $fillable = ['user_id', 'type', 'delivery_method', 'scheduled_date'];
+
+    public function user()
+    {
+      return $this->belongsTo(User::class);
+    }
+}
