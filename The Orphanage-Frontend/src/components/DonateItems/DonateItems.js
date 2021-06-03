89e3a247534ad4c920612f284fc7a88b75e0aba4
Seif Eldin Mohamed <s.seif26@yahoo.com>
2021-06-03 16:45:05 +0200

    update item selection

--- The Orphanage-Frontend/src/components/DonateItems/DonateItems.js
+++ The Orphanage-Frontend/src/components/DonateItems/DonateItems.js
@@ -4,133 +4,180 @@ import furnImage from "../../assets/furniture.png";
 import clothesImage from "../../assets/clothes.png";
 import booksImage from "../../assets/books.png";
 import "./DonateItems.css";
-import { Form } from 'react-bootstrap';
-import * as actions from '../../store/actions/index';
-import { connect } from 'react-redux';
+import { Form } from "react-bootstrap";
+import * as actions from "../../store/actions/index";
+import { connect } from "react-redux";
 import DatePicker from "react-datepicker";
-
+import Input from "../Input/Input";
 import "react-datepicker/dist/react-datepicker.css";
 
 class DonateItems extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            donationItems: [
-                {
-                    icon: foodImage,
-                    name: "Food",
-                    selected: false
-                },
-                {
-                    icon: clothesImage,
-                    name: "Clothes",
-                    selected: false
-                },
-                {
-                    icon: furnImage,
-                    name: "Furniture",
-                    selected: false
-                },
-                {
-                    icon: booksImage,
-                    name: "Books",
-                    selected: false
-                }
-            ],
-            deliveryMethod: 0,
-            scheduled_date: new Date()
-        };
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      donationItems: [
+        {
+          icon: foodImage,
+          name: "Food",
+          selected: false,
+          amount: 0,
+        },
+        {
+          icon: clothesImage,
+          name: "Clothes",
+          selected: false,
+          amount: 0,
+        },
+        {
+          icon: furnImage,
+          name: "Furniture",
+          selected: false,
+          amount: 0,
+        },
+        {
+          icon: booksImage,
+          name: "Books",
+          selected: false,
+          amount: 0,
+        },
+      ],
+      deliveryMethod: 0,
+      scheduled_date: new Date(),
+    };
+  }
 
-    selectDeliveryMethod = (method) => {
-        this.setState({ deliveryMethod: method });
-    }
+  selectDeliveryMethod = (method) => {
+    this.setState({ deliveryMethod: method });
+  };
 
-    getSelectedItems = () => {
-        let selectedItems = '';
-        for (let item of this.state.donationItems) {
-            if (item.selected) {
-                selectedItems += item.name + ',';
-            }
-        }
-        return selectedItems;
+  getSelectedItems = () => {
+    let selectedItems = [];
+    for (let item of this.state.donationItems) {
+      if (item.selected) {
+        selectedItems.push({
+            id: item.id,
+            amount: item.amount
+        })
+      }
     }
+    return selectedItems;
+  };
 
-    onDonate = () => {
-        let body = {
-            scheduled_date: this.state.scheduled_date,
-            type: this.getSelectedItems(),
-            delivery_method: this.state.deliveryMethod
-        }
-        this.props.onDonateItems(body);
-    }
+  onDonate = () => {
+    let body = {
+      scheduled_date: this.state.scheduled_date,
+      items: this.getSelectedItems(),
+      delivery_method: this.state.deliveryMethod,
+    };
+    console.log(body)
+    //this.props.onDonateItems(body);
+  };
 
-    render() {
-        return (
-            <div>
-                <div className="container">
-                    {this.state.donationItems.map((item, index) => {
-                        return (
-                            <div className="item" key={index} onClick={() => {
-                                let donationItems_new = [...this.state.donationItems];
-                                donationItems_new[index].selected = !donationItems_new[index].selected;
-                                this.setState({ donationItems: donationItems_new })
-                            }
-                            }>
-                                <img
-                                    src={item.icon}
-                                    alt="item"
-                                    data-testid="oud-logo-img"
-                                    className={(!this.state.donationItems[index].selected) ?
-                                        "item-image" : "item-image-border"}
-                                />
-                                <p className="item-name">Food</p>
-                            </div>
-                        );
-                    })}
-                </div>
-                <Form>
-                    {['radio'].map((type) => (
-                        <div key={`inline-${type}`} className="mb-3" className="radio-group">
-                            <Form.Check
-                                inline label="Home Pickup" name="group1" type={type} id={`inline-${type}-1`} className="radio"
-                                onChange={(event) => { this.selectDeliveryMethod(0) }}
-                            />
-                            <Form.Check
-                                inline label="Drop Yourself" name="group1" type={type} id={`inline-${type}-2`} className="radio"
-                                onChange={(event) => { this.selectDeliveryMethod(1) }}
-                            />
-                        </div>
-                    ))}
-                </Form>
-                    <div className="calender-con">
-                        <DatePicker
-                            selected={this.state.scheduled_date}
-                            onChange={(date) => this.setState({ scheduled_date: date })}
-                            showTimeSelect
-                            dateFormat="Pp"
-                            className="calender"
-                            popperClassName="popper"
-                        />
-                        <i className="fa fa-calendar mr-3 fa-lg sidebar-icons"></i>
-                    </div>                    
-                <button className="donate-btn" onClick={this.onDonate}>
-                    Donate
-                </button>
-            </div >
-        );
-    }
+  amountInputHandler = (event, itemNo) => {
+    let donationItems_new = [...this.state.donationItems];
+    donationItems_new[itemNo].amount = event.target.value;
+    this.setState({ donationItems: donationItems_new });
+  };
+
+  render() {
+    return (
+      <div>
+        <div className="container">
+          {this.state.donationItems.map((item, index) => {
+            return (
+              <div className="item" key={index}>
+                <img
+                  onClick={() => {
+                    let donationItems_new = [...this.state.donationItems];
+                    donationItems_new[index].selected =
+                      !donationItems_new[index].selected;
+                    this.setState({ donationItems: donationItems_new });
+                  }}
+                  src={item.icon}
+                  alt="item"
+                  data-testid="oud-logo-img"
+                  className={
+                    !this.state.donationItems[index].selected
+                      ? "item-image"
+                      : "item-image-border"
+                  }
+                />
+                <p className="item-name">{item.name}</p>
+                {this.state.donationItems[index].selected ? (
+                  <div className="amount-con">
+                    <p>Amount:</p>
+                    <Input
+                      type="number"
+                      min={0}
+                      onChange={(event) =>
+                        this.amountInputHandler(event, index)
+                      }
+                    />
+                  </div>
+                ) : null}
+              </div>
+            );
+          })}
+        </div>
+        <Form>
+          {["radio"].map((type) => (
+            <div
+              key={`inline-${type}`}
+              className="mb-3"
+              className="radio-group"
+            >
+              <Form.Check
+                inline
+                label="Home Pickup"
+                name="group1"
+                type={type}
+                id={`inline-${type}-1`}
+                className="radio"
+                onChange={(event) => {
+                  this.selectDeliveryMethod(0);
+                }}
+              />
+              <Form.Check
+                inline
+                label="Drop Yourself"
+                name="group1"
+                type={type}
+                id={`inline-${type}-2`}
+                className="radio"
+                onChange={(event) => {
+                  this.selectDeliveryMethod(1);
+                }}
+              />
+            </div>
+          ))}
+        </Form>
+        <div className="calender-con">
+          <DatePicker
+            selected={this.state.scheduled_date}
+            onChange={(date) => this.setState({ scheduled_date: date })}
+            showTimeSelect
+            dateFormat="Pp"
+            className="calender"
+            popperClassName="popper"
+          />
+          <i className="fa fa-calendar mr-3 fa-lg sidebar-icons"></i>
+        </div>
+        <button className="donate-btn" onClick={this.onDonate}>
+          Donate
+        </button>
+      </div>
+    );
+  }
 }
 
-const mapStateToProps = state => {
-    return {
-    };
+const mapStateToProps = (state) => {
+  return {};
 };
 
-const mapDispatchToProps = dispatch => {
-    return {
-        onDonateItems: (body) => dispatch(actions.donateItem(body))
-    };
+const mapDispatchToProps = (dispatch) => {
+  return {
+    onDonateItems: (body) => dispatch(actions.donateItem(body)),
+  };
 };
 
 export default connect(mapStateToProps, mapDispatchToProps)(DonateItems);
